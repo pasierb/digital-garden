5f3c437a578a761b7b89571c518c83e56f50956e
Michal Pasierbski <michal.pasierbski@ef.com>
2020-06-20 19:09:25 +0200

    Add fix intro typos

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,9 @@ const IndexPage = () => (
     <p>My name is Michał Pasierbski. I'm a software engineer at Amazon Web Services.</p>
 
     <p>
-      This website is my personal space where I dump my thoughts, mostly about engineering and self improvement.
+      This website is my personal space where I dump thoughts, mostly about engineering and self improvement.
       Writings that you will find here are sometimes raw, unpolished, even "work in progress".
-      This format is inspired by idea of <a href="https://joelhooks.com/digital-garden">digital garden</a>.
+      This format is inspired by the idea of a <a href="https://joelhooks.com/digital-garden">digital garden</a>.
     </p>
 
     <p>You can find more of me at <a href="https://github.com/pasierb">github</a>, <a href="https://twitter.com/mpasierbski">twitter</a>, <a href="https://www.linkedin.com/in/mpasierbski/">linkedin</a> and <a href="https://www.instagram.com/mpasierbski/">instagram</a>.</p>

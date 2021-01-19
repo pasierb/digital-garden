9cddb5e1a2db58ce32761b718a68b75069828627
Michal Pasierbski <mpasierb@amazon.com>
2021-01-19 19:25:44 +0100

    Fix utterances render

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,23 +1,35 @@
-import React, { useRef } from "react"
+import React, { useRef, useEffect } from "react"
+
+const CONFIG = {
+  repo: "pasierb/digital-garden",
+  label: "Comment",
+  theme: "github-light",
+  "issue-term": "title"
+};
 
 const Comments = () => {
-  const ref = useRef<HTMLScriptElement>();
-  const config = {
-    repo: "pasierb/digital-garden",
-    label: "Comment",
-    theme: "github-light",
-    crossOrigin: "anonymous",
-    "issue-term": "title"
-  }
+  const ref = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (ref.current.childNodes.length > 0) {
+      return;
+    }
+
+    const scriptElement = document.createElement('script')
+    scriptElement.setAttribute("src", "https://utteranc.es/client.js");
+    scriptElement.setAttribute("async", "");
+    scriptElement.setAttribute("crossorigin", "anonymous");
+
+    for (const key in CONFIG) {
+      scriptElement.setAttribute(key, CONFIG[key]);
+    }
+
+    ref.current.appendChild(scriptElement);
+  }, []);
 
   return (
-    <script
-      ref={ref}
-      src="https://utteranc.es/client.js"
-      {...config}
-      async
-    ></script>
-  )
+    <div ref={ref} />
+  );
 }
 
 export default Comments
